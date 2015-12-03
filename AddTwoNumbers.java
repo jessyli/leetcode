2dd13dff7b319f79d4ee125e492a9fbba138a685
Jessy Li <jessy.li999@gmail.com>
2015-12-03 14:35:31 -0500

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,6 +3,8 @@ You are given two linked lists representing two non-negative numbers. The digits
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 
+
+time complexity O(n)
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,6 +13,7 @@ Output: 7 -> 0 -> 8
  *     ListNode(int x) { val = x; }
  * }
  */
+ 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode newhead = new ListNode(0);
