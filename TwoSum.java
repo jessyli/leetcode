03fc011e18d9b2897c69f4fde4f44e7cdf02de6e
Jessy Li <jessy.li999@gmail.com>
2015-12-03 14:22:05 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,17 +1,18 @@
-// Given an array of integers, find two numbers such that they add up to a specific target number.
+Given an array of integers, find two numbers such that they add up to a specific target number.
 
-// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
 
-// You may assume that each input would have exactly one solution.
+You may assume that each input would have exactly one solution.
 
-// Input: numbers={2, 7, 11, 15}, target=9
-// Output: index1=1, index2=2
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
 
-// Use HashMap to store the target value.
+Use HashMap to store the target value.
 
-// Time complexity depends on the put and get operations of HashMap which is normally O(1).
+Time complexity depends on the put and get operations of HashMap which is normally O(1).
 
-// Time complexity of this solution is O(n).
+Time complexity of this solution is O(n).
+
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
