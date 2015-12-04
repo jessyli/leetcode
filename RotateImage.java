cc3eb9a7a27998e66872bc4b7cce6a5aedc9bb59
Jessy Li <jessy.li999@gmail.com>
2015-12-04 16:59:17 -0500

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -14,7 +14,6 @@ public class Solution {
     public static int[][] transform1(int[][] matrix, int n){
 		for(int i=0; i<n; i++){
 			for(int j=i+1; j<n; j++){
-//				swap(matrix[i][j], matrix[j][i]);
 				
 				int temp;
 				temp = matrix[i][j];
@@ -28,7 +27,6 @@ public class Solution {
 	public static int[][] transform2(int[][] matrix, int n){
 		for(int j=0; j<n/2; j++){
 			for(int i=0; i<n; i++){
-//			swap(matrix[i][j],matrix[n-1-i][j]);
 				int temp;
 				temp = matrix[i][j];
 				matrix[i][j] = matrix[i][n-1-j];
@@ -37,9 +35,5 @@ public class Solution {
 		}
 		return matrix;
 	}
-	public static void swap(int a, int b){
-		int temp =a;
-		b=a;
-		a=temp;
-	}
+	
 }
