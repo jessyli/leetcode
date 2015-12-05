d3d05cd55aa94e2e9c7a1486fe4bdc598a64483b
Jessy Li <jessy.li999@gmail.com>
2015-12-05 15:51:15 -0500

    Create MaximumSubarray.java

---
+++ MaximumSubarray.java
@@ -0,0 +1,26 @@
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+More practice:
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==0||nums==null){
+            return 0;
+        }
+        int[] localmax = new int[nums.length];
+        localmax[0]=nums[0];
+        int globalmax = nums[0];
+        for(int i=1; i<nums.length; i++){
+            
+                localmax[i] = localmax[i-1]>0?nums[i]+localmax[i-1]:nums[i];
+               
+                globalmax = Math.max(globalmax, localmax[i]);
+            
+        }
+        return globalmax;
+    }
+}
