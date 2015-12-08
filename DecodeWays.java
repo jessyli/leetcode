ac8830a0e99e9f239524a4313c14f21c4f131db3
Jessy Li <jessy.li999@gmail.com>
2015-12-08 16:25:38 -0500

    Update DecodeWays.java

--- DecodeWays.java
+++ DecodeWays.java
@@ -13,17 +13,38 @@ The number of ways decoding "12" is 2.
 
 public class Solution {
     public int numDecodings(String s) {
-       if (s == null || s.length() == 0) return 0;
-        int len = s.length();
-        int prev1 = 1;
-        int prev2 = s.charAt(0) == '0' ? 0 : 1;
-        for (int i = 2; i <= len; i++) {
-            int code1 = Integer.valueOf(s.substring(i - 1, i)); // 1 digit
-            int code2 = Integer.valueOf(s.substring(i - 2, i)); // 2 digits
-            int temp = prev2;
-            prev2 = (code1 != 0 ? prev2 : 0) + (code2 <= 26 && code2 > 9 ? prev1 : 0);
-            prev1 = temp;
+       if(s==null||s.length()==0||s=="0"){
+           return 0;
+       }
+       int[] result = new int[s.length()+1];
+       result[0] = 1;
+       if(s.charAt(0)=='0'){
+           result[1] = 0;
+       }
+       else{
+           result[1] = 1;
+       }
+       for(int i=2; i<=s.length(); i++){
+           if(isValide(s.substring(i-1, i))){
+               result[i] = result[i-1];
+           }
+           if(isValide(s.substring(i-2, i))){
+               result[i] = result[i-2]+result[i];
+           }
+       }
+       return result[s.length()];
+    }
+    
+    public boolean isValide(String s){
+        if(s.charAt(0)=='0'){
+            return false;
         }
-        return prev2;
+        int Value = Integer.parseInt(s);
+        if(Value>=1 && Value<=26){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
