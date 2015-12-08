99ed8eff614d493ee58b7313ca4f949ca6e2d1d2
Jessy Li <jessy.li999@gmail.com>
2015-12-08 13:56:07 -0500

    Update SetMatrixZeroes.java

--- SetMatrixZeroes.java
+++ SetMatrixZeroes.java
@@ -30,3 +30,53 @@ public class Solution {
 		}
     }
 }
+
+
+
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstrowzero = false;
+        boolean firstcolumnzero = false;
+        if(matrix == null){
+            return;
+        }
+        for(int i=0; i<matrix.length; i++){
+            if(matrix[i][0]==0){
+                firstcolumnzero = true;
+                break;
+            }
+        }
+        for(int i=0; i<matrix[0].length; i++){
+            if(matrix[0][i]==0){
+                firstrowzero = true;
+                break;
+            }
+        }
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                    
+                }
+            }
+        }
+        if(firstrowzero==true){
+            for(int i=0; i<matrix[0].length; i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(firstcolumnzero==true){
+            for(int i=0; i<matrix.length; i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
