bca64c2d484e9b042e301617700c9a94bd2d5172
Jessy Li <jessy.li999@gmail.com>
2015-12-12 01:20:16 -0500

    Update ConvertSortedListtoBinarySearchTree.java

--- ConvertSortedListtoBinarySearchTree.java
+++ ConvertSortedListtoBinarySearchTree.java
@@ -16,30 +16,35 @@
  * }
  */
 public class Solution {
+    static ListNode h;
     public TreeNode sortedListToBST(ListNode head) {
-    if(head == null)
-        return null;
-    ListNode cur = head;
-    int count = 0;
-    while(cur!=null)
-    {
-        cur = cur.next;
-        count++;
+        if(head == null){
+            return null;
+        }
+        int count = 0;
+        ListNode cur = head;
+        while(cur!=null){
+            cur = cur.next;
+            count++;
+        }
+        h = head;
+        return helper(0, count-1);
+      
     }
-    ArrayList<ListNode> list = new ArrayList<ListNode>();
-    list.add(head);
-    return helper(list,0,count-1);
-}
-private TreeNode helper(ArrayList<ListNode> list, int l, int r)
-{
-    if(l>r)
-        return null;
-    int m = (l+r)/2;
-    TreeNode left = helper(list,l,m-1);
-    TreeNode root = new TreeNode(list.get(0).val);
-    root.left = left;
-    list.set(0,list.get(0).next);
-    root.right = helper(list,m+1,r);
-    return root;
-}
+    public TreeNode helper(int l, int r){
+        if(l>r){
+            return null;
+        }
+        
+        int mid = (l+r)/2;
+        TreeNode left = helper(l, mid-1);
+        TreeNode root = new TreeNode(h.val);
+        h = h.next;
+        TreeNode right = helper(mid+1, r);
+        root.left = left;
+        root.right = right;
+        
+        return root;
+        
+    }
 }
